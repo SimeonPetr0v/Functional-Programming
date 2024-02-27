834c3a4b2890892408313318ec735968e2c0509c
SimeonPetr0v <simeonp925@gmail.com>
2024-02-27 22:14:58 +0200

    Homework

---
+++ Applied Arithmetic/Applied Arithmetic/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Applied_Arithmetic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            string input = Console.ReadLine();
+            while (input != "end")
+            {
+                Func<int, int> convertFunc = null;
+                if (input == "add")
+                    convertFunc = i => i + 1;
+                
+                else if (input == "subtract")
+                    convertFunc = i => i - 1;
+                
+                else if (input == "multiply")
+                    convertFunc = i => i * 2;
+               
+                if (input == "print")
+                    Console.WriteLine(string.Join(" ", array));
+                
+                else
+                    array = array.Select(convertFunc).ToArray();
+                
+            }
+        }
+    }
+}
---
+++ Reverse and Exclude/Reverse and Exclude/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Reverse_and_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int num = int.Parse(Console.ReadLine());
+
+            Predicate<int> divisible = i => i % num == 0;
+            Func<int[], Predicate<int>, int[]> remove = (ints, predicate) => ints.Where(i => !predicate(i)).ToArray();
+            Func<int[], int[]> reverse = ints => ints.Reverse().ToArray();
+            array = remove(array, divisible);
+            array = reverse(array);
+            
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
